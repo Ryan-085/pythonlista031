baed2628423cc1a9be6e0a170587b3ff8ef838c7
ART0059233 <ryanvirginio@gmail.com>
2023-08-02 11:43:37 -0300

    Initial commit

---
+++ questao01.py
@@ -0,0 +1,14 @@
+'''
+1) Desenvolver um programa que pergunte ao usuário o seu nome completo e seu sexo. Em seguida, o programa
+deve apresentar os dados anteriormente informados.
+'''
+
+name = input("qual o seu nome?")
+
+print("olá,", name, ".")
+
+print(type(name))
+
+sex = input("Qual é o seu sexo?")
+
+print("Muito obrigado por confiar em nós")
---
+++ questao02.py
@@ -0,0 +1,29 @@
+'''
+2) Elaborar um programa que pergunte quatro valores inteiros e apresente 2 resultados:
+a) Resultado de suas adições
+b) Resultado de suas multiplicações
+'''
+
+num1 = int(input("informe um número"))
+num2 = int(input("informe um número"))
+num3 = int(input("informe um número"))
+num4 = int(input("informe um número"))
+
+resultado1 = (num1 + num2 + num3 + num4)
+
+print("seu resultado é: ", resultado1)
+
+resulltado2 = (num1 + num2 + num3 + num4)
+
+print("seu resultado é: ", resultado2)
+
+resultado3 = (num1 + num2 + num3 + num4)
+
+print("seu resultado é: ", resultado3)
+
+resultado4 = (num1 + num2 + num3 + num4)
+
+print("seu resultado é: ", resultado4)
+
+
+
